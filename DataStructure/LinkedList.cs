ddafce496a9287f6e09dd5daaa778d9508a37dda
Patil-94 <ankitakhairnar10@.com>
2021-05-03 22:33:51 +0530

    [Ankita] Add:Linked list By Appending

--- DataStructure/LinkedList.cs
+++ DataStructure/LinkedList.cs
@@ -9,9 +9,9 @@ namespace DataStructure
     class LinkedList
     {
         public Node head;
-        public void InsertLast(int new_data)//creat method InserTlast 
+        public void InsertLast(int data)//creat method InserTlast 
         {
-            Node node = new Node(new_data);//
+            Node node = new Node(data);//
             if (this.head == null)
             {
                 this.head = node;
@@ -33,7 +33,13 @@ namespace DataStructure
             }
             return temp;
         }
-
+        public void InsertFront(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = this.head;
+            this.head = new_node;
+            Console.WriteLine("inserted into front" + new_node);
+        }
         internal void Display()
         {
             Node temp = this.head;
@@ -52,5 +58,34 @@ namespace DataStructure
                 Console.WriteLine(temp.data);
             }
         }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
     }
 }
+
